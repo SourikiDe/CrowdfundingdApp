80395603f70ec8e3411ef17e856a1ed99d932603
SourikiDe <sourikide05@gmail.com>
2026-03-25 13:50:04 +0530

    Update from StellarIDE - 2026-03-25T08:20:00.034Z

---
+++ client/next.config.ts
@@ -0,0 +1,7 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  allowedDevOrigins: ['svc-707ff750-morphvm-6q1s824w.http.cloud.morph.so'],
+};
+
+export default nextConfig;
